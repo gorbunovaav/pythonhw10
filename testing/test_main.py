d7a1a934c8e680d886545088e1b7b1c9ac0c6e5d
Anna Gorbunova <81052418+gorbunovaav@users.noreply.github.com>
2024-12-01 11:46:44 +0300

    first commit

---
+++ testing/test_main.py
@@ -0,0 +1,12 @@
+def test_sum():
+    assert 1 + 2 == 3
+
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_about(client):
+    response = client.get('/about/')
+    assert response.status_code == 200
